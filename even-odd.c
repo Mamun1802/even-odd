eec1c517db97cc07294537e01813b655c558ab9c
MAMUN <almamun1802@gmail.com>
2024-09-03 11:27:55 +0600

    modification and comments added

--- even-odd.c
+++ even-odd.c
@@ -5,18 +5,32 @@ int main()
 {
     int num1,num2,i,count1=0,count2=0;
     int evenPrint=0,oddPrint=0;
+
+    //Input the range
     printf("Enter the beginning number:");
     scanf("%d",&num1);
     printf("Enter the ending number:");
     scanf("%d",&num2);
 
+    //Check the validation of range
+      if (num1 > num2) {
+        printf("Invalid range. The beginning number should be less than or equal to the ending number.\n");
+        return 1;
+    }
+
+     //Check if both numbers are equal
+     if(num1==num2){
+        printf("The beginning and ending number are equal");
+     }
+
+     //Loop through the range to check even or odd
     for(i=num1; i<=num2; i++)
     {
         if(i%2==0)
         {
             if(evenPrint==0)
             {
-                printf("\nEven numbers = ");
+                printf("\n\nEven numbers = ");
                 evenPrint=1;
             }
             printf("%d ",i);
@@ -39,10 +53,9 @@ int main()
 
 
     }
-     printf("\n \n");
-    printf("Total even numbers = %d",count1);
-     printf("\n \n");
-    printf("Total odd numbers = %d",count2);
+     //Print the total
+    printf("\n\nTotal even numbers = %d",count1);
+    printf("\n\nTotal odd numbers = %d",count2);
 
     return 0;
 }
